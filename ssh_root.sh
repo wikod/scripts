6518e28607f1d02c9e21576c35e93cb357af08a0
Kevin Cai <caizhifei2003@163.com>
2023-11-25 16:47:16 +0800

    update

--- ssh_root.sh
+++ ssh_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-passwd \
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
-service ssh restart
+sudo passwd \
+&& sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
+&& service ssh restart
